ec06f0a47587a20df0bfb959a4b9d3ed8c150962
NoDeath <liinuz@live.se>
2020-05-02 01:15:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gameupdates
+A python script that will crawl (for now) Battlefield V's news page for new updates and news. It will then post it to a twitter bot account.
---
+++ checker.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests
+import pyshorteners
+import time
+import tweepy
+
+# Setting Api keys
+
+auth = tweepy.OAuthHandler("consumer_key",
+                           "consumer_secret")
+auth.set_access_token("Access_token",
+                      "Access_token_secret")
+api = tweepy.API(auth)
+
+s = pyshorteners.Shortener(api_key='bit.ly API secret')
+bfv_seen = set()
+
+# Check twitter api authentication
+try:
+    api.verify_credentials()
+    print("Twitter Authentication OK")
+except:
+    print("Error during authentication")
+
+
+def start():
+    # Battlefield V
+    page = requests.get(
+        'https://www.ea.com/games/battlefield/battlefield-5/news')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    grid = soup.find("ea-grid")
+    news = grid.find(slot="container")
+    copy = news.find(slot="copy").text
+    title = news.find("h3").text
+    link = news.find("a")
+    url = s.bitly.short("https://www.ea.com" + link.get('href'))
+    print("Checking Battlefield updates...")
+    with open('last_seen.txt') as f:
+        if link.get('href') not in f.read():
+            f = open("last_seen.txt", "w+")
+            f.write(link.get('href'))
+            bfv_seen.add(link.get('href'))
+            print("Sending tweet...")
+            api.update_status(f"{title} \n #BattlefieldV {url}")
+            f.close()
+
+
+while True:
+    start()
+    time.sleep(60)
