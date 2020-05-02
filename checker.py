4e3c06d1ee484414c8cd8baa6366e92342494ebf
NoDeath <liinuz@live.se>
2020-05-02 16:58:37 +0200

    Better error catching

--- checker.py
+++ checker.py
@@ -26,63 +26,82 @@ except:
 
 def start():
 
-     # Battlefield V
-    page = requests.get(
-        'https://www.ea.com/games/battlefield/battlefield-5/news')
-    soup = BeautifulSoup(page.text, 'html.parser')
-    grid = soup.find("ea-grid")
-    news = grid.find(slot="container")
-    copy = news.find(slot="copy").text
-    title = news.find("h3").text
-    link = news.find("a")
-    url = s.bitly.short("https://www.ea.com" + link.get('href'))
-    now = datetime.datetime.now()
-    print("Time:", now.strftime("%Y-%m-%d %H:%M:%S"))
-    print("Checking Battlefield updates...")
-    with open('seen/bfv_seen.txt') as f:
-        if link.get('href') not in f.read():
-            try:
-                f = open("seen/bfv_seen.txt", "w+")
-                f.write(link.get('href'))
-                print("Sending tweet...")
-                api.update_status(f"{title} \n #BattlefieldV {url}")
-                f.close()
-                pass
-            except tweepy.TweepError as e:
-                print(e)
-                time.sleep(60)
-                pass
+    # Battlefield V
+    try:
+        page = requests.get(
+            'https://www.ea.com/games/battlefield/battlefield-5/news')
+        soup = BeautifulSoup(page.text, 'html.parser')
+        grid = soup.find("ea-grid")
+        news = grid.find(slot="container")
+        copy = news.find(slot="copy").text
+        title = news.find("h3").text
+        link = news.find("a")
+        url = s.bitly.short("https://www.ea.com" + link.get('href'))
+    except AttributeError as e:
+        print("Battlefield check error:", e)
+        pass
+    except pyshorteners.exceptions as e:
+        print(e)
+        pass
+    else:
+        print("Checking Battlefield updates...")
+        with open('seen/bfv_seen.txt') as f:
+            if link.get('href') not in f.read():
+                try:
+                    f = open("seen/bfv_seen.txt", "w+")
+                    f.write(link.get('href'))
+                    print("Sending tweet...")
+                    api.update_status(f"{title} \n #BattlefieldV {url}")
+                    f.close()
+                    pass
+                except tweepy.TweepError as e:
+                    print(e)
+                    time.sleep(60)
+                    pass
 
     # Risk of Rain 2
-    page = requests.get(
-        'https://store.steampowered.com/news/?appids=632360')
-    soup = BeautifulSoup(page.text, 'html.parser')
-    news = soup.find(id="news")
-    title = news.find(class_="posttitle").text
-    link = news.find("a")
-    url = s.bitly.short(link.get("href"))
-    print("Checking Risk of Rain 2 updates...")
-    with open('seen/ror2_seen.txt') as f:
-        if link.get('href') not in f.read():
-            try:
-                f = open("seen/ror2_seen.txt", "w+")
-                f.write(link.get('href'))
-                print("Sending tweet...")
-                api.update_status(f"{title} \n #RoR2 {url}")
-                f.close()
-                pass
-            except tweepy.TweepError as e:
-                print(e)
-                time.sleep(60)
-                pass
+    try:
+        page = requests.get(
+            'https://store.steampowered.com/news/?appids=632360')
+        soup = BeautifulSoup(page.text, 'html.parser')
+        news = soup.find(id="news")
+        title = news.find(class_="posttitle").text
+        link = news.find("a")
+        url = s.bitly.short(link.get("href"))
+    except AttributeError as e:
+        print("Risk of Rain 2 check error:", e)
+        pass
+    except pyshorteners.exceptions as e:
+        print(e)
+        pass
+    else:
+        print("Checking Risk of Rain 2 updates...")
+        with open('seen/ror2_seen.txt') as f:
+            if link.get('href') not in f.read():
+                try:
+                    f = open("seen/ror2_seen.txt", "w+")
+                    f.write(link.get('href'))
+                    print("Sending tweet...")
+                    api.update_status(f"{title} \n #RoR2 {url}")
+                    f.close()
+                    pass
+                except tweepy.TweepError as e:
+                    print(e)
+                    time.sleep(60)
+                    pass
+                except AttributeError as e:
+                    print(e)
+                    pass
 
 
 while True:
     try:
+        now = datetime.datetime.now()
         requests.get("http://google.com")
         start()
         time.sleep(60)
         os.system('cls')
+        print("Time:", now.strftime("%Y-%m-%d %H:%M:%S"))
     except requests.exceptions.RequestException:
         print("Connection error, retrying in 5 minutes.")
         time.sleep(300)
